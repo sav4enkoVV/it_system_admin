5a646bc1639ee906c7bc07d6cd68fc6bfd6dcb1d
Valera <79515414890@yandex.ru>
2024-04-02 18:03:37 +0300

    update dfs file

--- dfs.py
+++ dfs.py
@@ -1,19 +1,26 @@
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
+def dfs(graph, start, length, visited=None):
+    try:
+        if visited is None:
+            visited = set()
+        visited.add(start)
+    except Exception as e:
+        print ("Node is not valid" + start)
+        raise e
 
     print(start)
+    print("Length is ", length)
 
     for next in graph[start] - visited:
-        dfs(graph, next, visited)
+        dfs(graph, next, length + 1, visited)
     return visited
-
 
 
 graph = {'0': set(['2']),
          '1': set(['3']),
-         '2': set(['0'])}
+         '2': set(['0']),
+         '3': set(['1'])}
 
 
-dfs(graph, '0')
+ln = 0
+
+dfs(graph, '5', ln)
